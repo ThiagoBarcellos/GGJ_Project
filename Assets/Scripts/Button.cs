ed14190e67ab30c7b229c2a43bb182940c39ef9b
Global Game Jam <GGJ2018@nave.local>
2018-01-27 17:36:52 -0200

    update 2.0

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+    public void OnClickStart() {
+        SceneManager.LoadScene("Main", LoadSceneMode.Additive);
+    }
+
+    public void OnClickHowToPlay() {
+        SceneManager.LoadScene("HowToPlay", LoadSceneMode.Additive);
+    }
+
+    public void OnClickCredits() {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/CanMoveTo.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanMoveTo : MonoBehaviour
+{
+
+    public int nEletrons;
+    public bool canMove = false;
+    public bool temElerons = false;
+    Vector3 targetPosition;
+    public GameObject ppotse;
+   
+    //public gameObject player;
+
+    void Start()
+    {
+        nEletrons = 0;
+        targetPosition = transform.position;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                targetPosition = hit.point;
+                this.transform.position = targetPosition;
+            }
+
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        coll.gameObject.GetComponentInChildren<MeshRenderer>().material.color = Color.green;
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        Debug.Log("ola");
+        if (coll.tag == "habil")
+        {
+            Debug.Log("ola2");
+            canMove = true;
+            coll.gameObject.GetComponentInChildren<MeshRenderer>().material.color = Color.green;
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        canMove = false;
+        coll.gameObject.GetComponentInChildren<MeshRenderer>().material.color = Color.white;
+    }
+
+    void OnMouseDown() {
+         if (gameObject.tag == "habil") {
+             //player.transform.position = vector3.lerp(player.transform.position, gameobject.transform.positions);
+         }
+     }
+}
